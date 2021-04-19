41b3f0f4f76af58ee63659ce988faa0113aeda3f
Alberto Massidda <alberto.massidda@uniroma3.it>
2021-04-19 19:02:25 +0200

    added branch autodetect fixed bug for which some lines in diff could be skipped and msg written anyway

--- build_dataset.py
+++ build_dataset.py
@@ -11,8 +11,8 @@ from nltk.tokenize import WordPunctTokenizer
 parser = argparse.ArgumentParser()
 parser.add_argument("repo_path", help="Path to the repo location on the filesystem")
 parser.add_argument("prefix", help="Input file prefix to search for <prefix>.diff and <prefix>.msg files")
-parser.add_argument("-b", "--branch", default="master", help="Git branch to scan")
-parser.add_argument("-nopos", "--no-pos-tagging", help="Skip POS tagging")
+parser.add_argument("-b", "--branch", default="auto", help="Git branch to scan (default: autodetect; fall back chaim: 'master', 'main', 'develop', then throws error")
+parser.add_argument("-nopos", "--no-pos-tagging", action="store_true", help="Skip POS tagging")
 parser.add_argument("-v", "--verbose", action="store_true", help="Suppress output")
 
 args = parser.parse_args()
@@ -23,6 +23,40 @@ verbose = args.verbose
 nopos = args.no_pos_tagging
 branch = args.branch
 
+print(f"Parsing {repo_path}, ", end='', flush=True)
+
+if branch == "auto":
+
+    print(f"detecting branch...", end='', flush=True)
+
+    found_default_branch = False
+
+    for default_branch in ['master', 'main', 'develop']:
+
+        if found_default_branch:
+            break
+
+        try:
+            repo = RepositoryMining(repo_path, only_in_branch='master', only_no_merge=True)
+
+            for commit in repo.traverse_commits():
+                check = commit.msg
+
+                if check is not None:
+                    found_default_branch = True
+                    branch = default_branch
+                    print(f"Found {branch} branch, will use this", flush=True)
+                    break
+
+        except git.exc.GitCommandError as gce:
+            pass
+
+    if not found_default_branch:
+        print(f"Can't detect default branch", flush=True)
+        sys.exit(1)
+else:
+    print(f"branch {branch}", flush=True)
+
 output_diff = f"{filename_prefix}.diff"
 output_msg = f"{filename_prefix}.msg"
 
@@ -31,6 +65,7 @@ regex_issue = re.compile("#[0-9]+")
 
 tokenizer = WordPunctTokenizer()
 nlp = spacy.load("en_core_web_sm")
+
 
 repo = RepositoryMining(repo_path, only_in_branch=branch, only_no_merge=True)
 
@@ -43,7 +78,6 @@ if os.path.isfile(output_msg):
 total = 0
 added = 0
 
-print(f"Parsing {repo_path}, branch {branch}", flush=True)
 
 with open(output_diff,"a+") as fp_diff_out:
     with open(output_msg,"a+") as fp_msg_out:
@@ -71,8 +105,6 @@ with open(output_diff,"a+") as fp_diff_out:
                         print("s", end='')
                     continue
 
-            fp_msg_out.write(msg + "\n")
-
             diff_line = []
 
             for i, modified_file in enumerate(commit.modifications): # here you have the list of modified files
@@ -97,9 +129,11 @@ with open(output_diff,"a+") as fp_diff_out:
                 print("-", end='')
                 continue
 
+
             diff_line_str = ' '.join(diff_line).strip()
             diff_line_str = ' '.join([token.strip() for token in tokenizer.tokenize(diff_line_str)]).replace("< nl >", "<nl>")
 
+            fp_msg_out.write(msg + "\n")
             fp_diff_out.write(diff_line_str + "\n")
 
             added += 1
