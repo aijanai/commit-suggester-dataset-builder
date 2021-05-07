726f4055ca9adf2c1558c60e0b7cebde57575499
Alberto Massidda <alberto.massidda@uniroma3.it>
2021-05-07 18:12:16 +0200

    added configurable prefix in split script added overridable num of tokens in diff for generation script

--- parallel_generation.sh
+++ parallel_generation.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 if [[ $# -lt 1 ]]; then
-	echo "Usage: $0 <repos dir> <datasets dir>"
+	echo "Usage: $0 <repos dir> <datasets dir> <diff number of tokens>"
 	exit 1
 fi
 
+if [[ -z $3 ]]; then
+	TOKENS=150
+else
+	TOKENS=$3
+fi
+
+echo "Using $TOKENS tokens"
+
 mkdir -p $2
 
-ls $1 |parallel -j 80 ./git2bitext.py -A -E $1/{} $2/{}
+ls $1 |parallel -j 80 ./git2bitext.py -D $TOKENS -A -E $1/{} $2/{}
--- parallel_split.sh
+++ parallel_split.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [[ $# -lt 1 ]]; then
-	echo "Usage: $0 <datasets dir>"
+if [[ $# -lt 2 ]]; then
+	echo "Usage: $0 <datasets dir> <prefix>"
 	exit 1
 fi
 
 ls $1| sed 's/\.msg$//g' | sed 's/\.diff$//g' | sort -u |parallel -j 30 ./split_test_train_valid.py -v $1/{}
 
 
-cat $1/*.valid.msg | tr A-Z a-z > dataset.valid.msg
-cat $1/*.valid.diff | tr A-Z a-z > dataset.valid.diff
-cat $1/*.test.diff | tr A-Z a-z > dataset.test.diff
-cat $1/*.test.msg | tr A-Z a-z > dataset.test.msg
-cat $1/*.train.msg | tr A-Z a-z > dataset.train.msg
-cat $1/*.train.diff | tr A-Z a-z > dataset.train.diff
+cat $1/*.valid.msg | tr A-Z a-z > $2.valid.msg
+cat $1/*.valid.diff | tr A-Z a-z > $2.valid.diff
+cat $1/*.test.diff | tr A-Z a-z > $2.test.diff
+cat $1/*.test.msg | tr A-Z a-z > $2.test.msg
+cat $1/*.train.msg | tr A-Z a-z > $2.train.msg
+cat $1/*.train.diff | tr A-Z a-z > $2.train.diff
